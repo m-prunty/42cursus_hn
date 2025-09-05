c488e8667aab34ba602ba84027a51c93dff2b9ad
potz <marcusprunty@gmail.com>
2025-09-05 15:27:48 +0200

    a clean starting dir for all 42 projects

---
+++ rank_00/libft/libft.h
@@ -0,0 +1,22 @@
+/*
+• toupper
+• isalpha
+• isdigit
+• tolower
+• isalnum
+• isascii• strchr
+• isprint• strrchr
+• strlen
+• strncmp
+• memset
+• bzero• memchr
+• memcpy• memcmp
+• memmove
+• strnstr
+• strlcpy
+• strlcat
+• atoi
+In order to implement the two following functions, you will use malloc():
+• calloc
+• strdup
+*/
