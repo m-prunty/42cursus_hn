08ab27beeea2027c65b76e1e14a94c42666b478b
maprunty <maprunty@student.42heilbronn.de>
2025-10-24 23:26:16 +0200

    kaboooom: works for some input and blows up on more

--- rank_01/get_next_line/get_next_line.c
+++ rank_01/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: maprunty <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:36:23 by maprunty          #+#    #+#             */
-/*   Updated: 2025/10/23 22:32:39 by maprunty         ###   ########.fr       */
+/*   Updated: 2025/10/24 23:25:13 by maprunty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,15 @@ char	*clean_return(char **ptr)
 	char *tmp;
 	char *sub;
 
-	nl = ft_strchr(*ptr, '\n') + 1;
+	nl = ft_strchr(*ptr, '\n');
 	if (nl)
 	{
 		tmp = *ptr;
-		sub = ft_substr(*ptr, 0, nl - *ptr);
-		*ptr = ft_substr(*ptr, nl - *ptr, ft_strlen(*ptr) - (nl - *ptr));
+		sub = ft_substr(*ptr, 0, nl + 1 - *ptr);
+		if (ft_strlen(sub) < ft_strlen(*ptr))
+			*ptr = ft_substr(*ptr, nl + 1 - *ptr, ft_strlen(*ptr) - (nl + 1 - *ptr));
+		else
+			*ptr = "";
 		free(tmp);
 		return (sub);
 	}
@@ -35,24 +38,25 @@ char	*get_next_line(int fd)
 	static char	*ptr = "";
 	char		buf[BUFFER_SIZE];
 	char		*tmp;
-	size_t		nread;
+	int			nread;
 
 	nread = 1;
 	ft_bzero(buf, BUFFER_SIZE);
 	tmp = NULL;
-	if (!*ptr)
-		nread = read(fd, buf , BUFFER_SIZE);
-	//buf[nread] = '\0';
-	if (nread)
+	while ((!*ptr || !ft_strchr(ptr, '\n')) && nread)
 	{
-		tmp = ptr;
-		ptr = ft_strjoin(ptr, buf);
-		if (*tmp)
-			free(tmp);
+		nread = read(fd, buf, BUFFER_SIZE);
+		if (nread >= 1)
+		{
+			tmp = ptr;
+			ptr = ft_strjoin(ptr, buf);
+			if (*tmp)
+				free(tmp);
+		}
+		else if (nread < 0)
+			return (free(ptr), NULL);
 	}
 	if (*ptr)
 		return (clean_return(&ptr));
-	else
-		return (NULL);
-	return (get_next_line(fd));
+	return (NULL);
 }
