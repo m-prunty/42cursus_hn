7effbadfa75d3f4834c34f4986a42576a41c894c
maprunty <maprunty@student.42heilbronn.de>
2025-10-27 20:12:05 +0100

    nearly working: not normed and failing on strict memtests

--- rank_01/get_next_line/get_next_line.c
+++ rank_01/get_next_line/get_next_line.c
@@ -6,56 +6,72 @@
 /*   By: maprunty <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:36:23 by maprunty          #+#    #+#             */
-/*   Updated: 2025/10/27 17:27:59 by maprunty         ###   ########.fr       */
+/*   Updated: 2025/10/27 20:08:06 by maprunty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 char	*clean_return(char **ptr)
-{
-	char *nl;
-	char *tmp;
-	char *sub;
-	int	n;
+{	char	*nl;
+	char	*tmp;
+	char	*sub;
+	size_t	n;
 
-	nl = ft_strchr(*ptr, '\n');
+	tmp = *ptr;
+	*ptr = NULL;
+	nl = ft_strchr(tmp, '\n');
+	n = ft_strlen(tmp);
 	if (nl)
 	{
-		tmp = *ptr;
-		sub = ft_substr(*ptr, 0, nl + 1 - *ptr);
-		if (ft_strlen(sub) < ft_strlen(*ptr))
-			*ptr = ft_substr(*ptr, nl + 1 - *ptr, ft_strlen(*ptr) - (nl + 1 - *ptr));
+		sub = ft_substr(tmp, 0, nl + 1 - tmp);
+		if (!sub)
+			return (free(tmp), NULL);
+		if (ft_strlen(sub) < n)
+		{
+			*ptr = ft_substr(tmp, nl + 1 - tmp, ft_strlen(tmp) - (nl + 1 - tmp));
+			if (!tmp)
+				return (free(tmp), NULL);
+		}
 		else
-			*ptr = NULL;
+			tmp = NULL;
 		free(tmp);
 	}
 	else
 	{
-		n = ft_strlen(*ptr);
 		sub = malloc(sizeof(char) * n + 1);
-		ft_memcpy(sub, *ptr, n);
+		if (!sub)
+			return (free(tmp), NULL);
+		ft_memcpy(sub, tmp, n);
 		sub[n] = '\0';
-		free(*ptr);
+		free(tmp);
 		*ptr = NULL;
 	}
 	return (sub);
 }
 
-void	add_to_stat(char **ptr, char *buf, int nread)
+int	add_to_stat(char **ptr, char *buf, int *nread)
 {
-	int n;
+	int		n;
 	char	*tmp;
 
 	n = 0;
 	if (*ptr)
 		n = ft_strlen(*ptr);
-	tmp = (char *)malloc(sizeof(char) * (n + nread + 1));
+	tmp = (char *)malloc(sizeof(char) * (n + *nread + 1));
+	if (!tmp)
+	{
+		*nread = -1;
+		free(*ptr);
+		*ptr = NULL;
+		return (0);
+	}
 	ft_memcpy(tmp, *ptr, n);
-	ft_memcpy(tmp + n, buf, nread);
-	tmp[n + nread] = '\0';
+	ft_memcpy(tmp + n, buf, *nread);
+	tmp[n + *nread] = '\0';
 	free(*ptr);
 	*ptr = tmp;
+	return (1);
 }
 
 char	*get_next_line(int fd)
@@ -71,8 +87,8 @@ char	*get_next_line(int fd)
 	while ((!ptr || !ft_strchr(ptr, '\n')) && nread)
 	{
 		nread = read(fd, buf, BUFFER_SIZE);
-		if (nread >= 1)
-			add_to_stat(&ptr, buf, nread);
+		if (nread >= 1 && add_to_stat(&ptr, buf, &nread))
+			;
 		else if (nread < 0)
 		{
 			free(ptr);
