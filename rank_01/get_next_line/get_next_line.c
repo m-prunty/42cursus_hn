5460b71abd4d649faec04d9969ecd2a30fd824c5
maprunty <maprunty@student.42heilbronn.de>
2025-11-19 14:23:39 +0100

    gnl: rebase to previous working version with static malloc. Starting point for rewrite

--- rank_01/get_next_line/get_next_line.c
+++ rank_01/get_next_line/get_next_line.c
@@ -6,183 +6,100 @@
 /*   By: maprunty <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:36:23 by maprunty          #+#    #+#             */
-/*   Updated: 2025/11/08 00:26:37 by maprunty         ###   ########.fr       */
+/*   Updated: 2025/11/19 14:10:02 by maprunty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stddef.h>
-#include <unistd.h>
-#define CHUNK 1024
 
-typedef struct	s_block
+//int calloc_help(char *str, )
+
+void *free_null(char **ptr)
 {
-	struct s_block	*next;
-	void	*ptr;
-	size_t	size;
-	int		marked;
-}		t_block ;
-
-
-
-
-void *mt_calloc(t_block **head, size_t nmemb, size_t size)
-{
-	void	*p;
-	t_block *b;
-	
-	p =	ft_calloc(nmemb, size);
-	b = ft_calloc(sizeof(t_block), 1);
-	b->ptr = p;
-	b->size = nmemb * size;
-	b->marked = 0;
-	b->next = NULL;
-	if (!*head)
-		*head = b;
-	else
-	{
-		t_block *tmp = *head;
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp->next = b;
-	}
-	return (p);
+	free(*ptr);
+	*ptr = NULL;
+	return (NULL);
 }
 
-int	calloc_protect(t_block **head, char **to_calloc, size_t n)
-{
-	*to_calloc = mt_calloc(head, n, 1);
-	return (*to_calloc != NULL);
-}
-
-t_block	*mt_free_block(t_block *block)
-{
-	t_block *tmp;
-
-	tmp = block->next;
-	free(block->ptr);
-	free(block);
-	block = NULL;
-	return (tmp);
-}
-
-void	mt_free(t_block **head, void *ptr)
-{
-	t_block *tmp;
-	t_block *prev;
-
-	prev = NULL;
-	tmp = *head;
-	if (!ptr)
-	{
-		while (tmp)
-			tmp = mt_free_block(prev);
-		*head = NULL;
-		return ;
-	}
-	while (tmp && tmp->ptr != ptr)
-	{
-		prev = tmp;
-		tmp = tmp->next;
-	}
-	if (!tmp)
-		return;
-	if (prev)
-		prev->next = tmp->next;
-	else
-		*head = tmp->next;
-	mt_free_block(tmp);
-}
-
-char	*clean_return(t_block **mt, char **ptr)
-{
-	char	*ptr_ret;
-	char	*nl_ret;
-	size_t	n[2];
-
-	n[1] = ft_strlen(*ptr);
-	n[0] = 0;
-	ptr_ret = (char *)ft_memchr(*ptr, '\n', n[1]);
-	if (ptr_ret)
-		n[0] = (ptr_ret - *ptr + 1);
-	ptr_ret = NULL;
-	if (!n[0])
-		n[0] = n[1];
-	if (!calloc_protect(mt, &nl_ret, sizeof(char) * (n[0] + 1)))
-		return (mt_free(mt, NULL), NULL);
-	ft_memcpy(nl_ret, *ptr, n[0]);
-	if (n[0] < n[1])
-	{	
-		if (!calloc_protect(mt, &ptr_ret, sizeof(char) * (n[1] - n[0] + 1)))
-			return (mt_free(mt, NULL), NULL);
-		ft_memcpy(ptr_ret, *ptr + n[0], n[1] - n[0]);
-		ptr_ret[n[1] - n[0]] = '\0';
-	}
-	mt_free(mt, *ptr);
-	(*mt)->ptr = ptr_ret; 
-	if (*nl_ret)
-		return (nl_ret);
-	return (mt_free(mt, NULL) ,NULL);
-}
-
-
-
-int	add_to_stat(t_block **mt, char **ptr, char *buf, int *nread)
-{
-	size_t	cur_n;
-	size_t	sz;
+char	*clean_return(char **ptr)
+{	char	*nl;
+	char	*tmp;
+	char	*sub;
 	size_t	n;
+
+	tmp = *ptr;
+	*ptr = NULL;
+	n = ft_strlen(tmp);
+	nl = ft_memchr(tmp, '\n', n);
+	if (nl)
+	{
+		sub = ft_substr(tmp, 0, nl + 1 - tmp);
+		if (!sub)
+			return (free_null(&tmp), NULL);
+		if (ft_strlen(sub) < n)
+		{
+			*ptr = ft_substr(tmp, nl + 1 - tmp, ft_strlen(tmp) - (nl + 1 - tmp));
+			if (!*ptr)
+				return (free_null(&sub), free_null(&tmp),  NULL);
+		}
+		free(tmp);
+	}
+	else
+	{
+		sub = malloc(sizeof(char) * n + 1);
+		if (!sub)
+			return (free_null(&tmp), NULL);
+		ft_memcpy(sub, tmp, n);
+		sub[n] = '\0';
+		free(tmp);
+		tmp = NULL;
+	}
+	return (sub);
+}
+
+int	add_to_stat(char **ptr, char *buf, int *nread)
+{
+	int		n;
 	char	*tmp;
 
 	n = 0;
 	if (*ptr)
 		n = ft_strlen(*ptr);
-	sz = n + *nread + 1;
-	cur_n = (( n - 1 + CHUNK) / CHUNK);
-	if (!*ptr || sz + 1 >= CHUNK * (cur_n ))
+	tmp = (char *)malloc(sizeof(char) * (n + *nread + 1));
+	if (!tmp)
 	{
-		if (!calloc_protect(mt, &tmp, CHUNK * (cur_n + 1)))
-		{
-			*nread = -1;
-			return (0);
-		}
-		if (*ptr && ft_memcpy(tmp, *ptr, n))
-			mt_free(mt, ptr);
-		*ptr = tmp;
+		*nread = -1;
+		return (0);
 	}
-	ft_memcpy(*ptr + n, buf, *nread);
+	ft_memcpy(tmp, *ptr, n);
+	ft_memcpy(tmp + n, buf, *nread);
+	tmp[n + *nread] = '\0';
+	free(*ptr);
+	*ptr = tmp;
 	return (1);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_block	*mt; 
-	char			**ptr;
-	char			*buf;
-	int				nread;
+	static char	*ptr = NULL;
+	char		*buf;
+	int			nread;
 
 	nread = 1;
-	ptr = (char **)mt_calloc(&mt, 1, sizeof(char *));
-	buf = (char *)mt_calloc(&mt, BUFFER_SIZE + 1, sizeof(char));
-	if (!buf || !ptr)
-		return (mt_free(&mt, NULL), NULL);
-	if (mt->ptr && *(char *)mt->ptr)
-		*ptr = mt->ptr;
-	else
-		*ptr = (char *)mt_calloc(&mt, CHUNK, sizeof(char));
+	buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buf)
+		return (free_null(&ptr), NULL);
 	while ((!ptr || !ft_memchr(buf, '\n', nread)) && nread)
 	{
-		ft_bzero(buf, BUFFER_SIZE + 1);
 		nread = read(fd, buf, BUFFER_SIZE);
-		if (nread >= 1 && add_to_stat(&mt, ptr, buf, &nread))
+		if (nread >= 1 && add_to_stat(&ptr, buf, &nread))
 			;
 		else if (nread < 0)
-			return (mt_free(&mt, NULL), NULL);
+			return (free_null(&buf), free_null(&ptr),  NULL);
 	}
 	if (buf)
-		mt_free(&mt, buf);
+		free_null(&buf);
 	if (ptr || nread)
-		return (clean_return(&mt, ptr));
-	return (mt_free(&mt, NULL), NULL);
+		return (clean_return(&ptr));
+	return (NULL);
 }
-
--- rank_01/get_next_line/get_next_line_utils.c
+++ rank_01/get_next_line/get_next_line_utils.c
@@ -6,99 +6,12 @@
 /*   By: maprunty <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 16:03:04 by maprunty          #+#    #+#             */
-/*   Updated: 2025/11/07 23:16:25 by maprunty         ###   ########.fr       */
+/*   Updated: 2025/10/28 11:41:59 by maprunty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-void	*ft_calloc(size_t n, size_t size)
-{
-	void	*p;
-	size_t			i;
-
-	p = malloc(n * size);
-	if (!p)
-		return (NULL);
-	i = 0;
-	while (i < n)
-		((unsigned char *)p)[i++] = 0;
-	return (p);
-}
-
-
-void	ft_bzero(void *s, size_t n)
-{
-	size_t			i;
-	unsigned char	*p;
-
-	p = (unsigned char *)s;
-	i = 0;
-	while (i < n)
-		p[i++] = 0;
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i = 0;
-
-	if (!s)
-		return (0);
-	while (s[i])
-		i++;
-	return (i);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t			i;
-	unsigned char	*d;
-	const unsigned char	*s;
-
-	if (!dst && !src)
-		return (NULL);
-	d = (unsigned char *)dst;
-	s = (const unsigned char *)src;
-	i = 0;
-	while (i < n)
-	{
-		d[i] = s[i];
-		i++;
-	}
-	return (dst);
-}
-
-void	*free_null(char **p)
-{
-	if (p && *p)
-	{
-		free(*p);
-		*p = NULL;
-	}
-	return (NULL);
-}
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	const unsigned char	*us;
-	unsigned char		uc;
-
-	if (!s)
-		return (NULL);
-	us = (const unsigned char *)--s;
-	uc = (unsigned char)c;
-	if (n)
-	{
-		while (n-- && uc != *++us)
-			;
-		if (uc == *us)
-			return ((void *)us);
-	}
-	return (NULL);
-}
-
-
-/* 
 size_t  ft_strlen(const char *s)
 {
     size_t  i;
@@ -129,6 +42,99 @@ void    ft_bzero(void *s, size_t n)
 		*(char *)s++ = '\0';
 }   
 
+char    *ft_strchr(const char *s, int c)
+{
+    int i;
+    int n;
+
+    i = 0;
+    n = ft_strlen(s);
+    while (i <= n)
+    {   
+        if (*(s + i) == (char)c)
+            return ((char *)s + i); 
+        i++;
+    }   
+    return (NULL);
+}
+
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t  nsrc;
+    size_t  ndst;
+    size_t  cpy;
+
+    nsrc = ft_strlen(src);
+    ndst = ft_strlen(dst);
+    if (ndst >= size)
+        return (nsrc + size);
+    if (nsrc + ndst >= size)
+        cpy = size - ndst - 1;
+    else
+        cpy = nsrc;
+    if (size > 0)
+    {
+        ft_memcpy(dst + ndst, src, cpy);
+        dst[ndst + cpy] = '\0';
+    }
+    return (nsrc + ndst);
+}   
+size_t  ft_strlcpy(char *dst, const char *src, size_t size)
+{
+    size_t  n;
+    size_t  cpy;
+    
+    n = ft_strlen(src);
+    if (n >= size)
+        cpy = size - 1;
+    else
+        cpy = n;
+    if (size > 0)
+    {
+        ft_memcpy(dst, src, cpy);
+        dst[cpy] = '\0';
+    }
+    return (n);
+} 
+char    *ft_strjoin(char const *s1, char const *s2)
+{
+    char    *joint;
+    int     n1;
+    int     n2;
+
+    n1 = ft_strlen(s1);
+    n2 = ft_strlen(s2);
+    joint = (char *)malloc(sizeof(char) * (n1 + n2 + 1));
+    if (!joint)
+        return (NULL);
+    ft_strlcpy(joint, s1, n1 + 1);
+    ft_strlcat(joint, s2, n1 + n2 + 1);
+    return (joint);
+}
+
+
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char    *sub;
+    size_t  n;
+
+    n = ft_strlen(s);
+    if (start >= n || len == 0)
+	{
+		sub = (char *)malloc(sizeof(char));
+		if (!sub)
+			return (NULL);
+		ft_strlcpy(sub, "", 1);
+        return (sub);
+	}
+	if (start + len > n)
+        len = n - start;
+    sub = (char *)malloc(sizeof(char) * (len + 1));
+    if (!sub)
+        return (NULL);
+    ft_strlcpy(sub, s + start, len + 1);
+    return (sub);
+}
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void		*p;
@@ -142,6 +148,19 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ft_bzero((char *)p, n);
 	return (p);
 }
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*us;
+	unsigned char		uc;
 
-
-*/
+	us = (const unsigned char *)--s;
+	uc = (unsigned char)c;
+	if (n)
+	{
+		while (n-- && uc != *++us)
+			;
+		if (uc == *us)
+			return ((void *)us);
+	}
+	return (NULL);
+}
