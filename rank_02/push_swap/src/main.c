0ea2c9fcf7db0e2f49b7fbce8f59639057bed319
potz <marcusprunty@gmail.com>
2025-12-06 08:33:01 +0100

    ps: 1st steps, basic parsing, storing n ints in n array of t_elements, stack contains pointer to head of t_list. where t_list->content = t_element. bstree added at t_stack->root to presort

--- rank_02/push_swap/src/main.c
+++ rank_02/push_swap/src/main.c
@@ -61,16 +61,16 @@ typedef struct s_stack
 {
 	char	name;
 	int		n;
-	t_list	head;
-	t_list	tail;
-	t_btree root;
+	t_list	*head;
+	t_list	*tail;
+	t_btree *root;
 	
 }	t_stack;
 
 typedef struct s_element
 {
 	int		value;
-	int		sort_loc;
+	int		sort;
 }	t_element;
 
 
@@ -83,18 +83,83 @@ void	push_swap(int *iarr, int ac)
 
 }
 
-t_stack		*init_stack(int *iarr, int n, char name)
+t_element	*ps_elenew(int value)
 {
-	t_stack  stk;
+	t_element *element;
+
+	element = (t_element *)ft_calloc(1, sizeof(t_element));
+	if (!element)
+		return (NULL);
+	element->value = value;
+	return (element);
+}
+
+t_element		*ps_getelements(int *iarr, int n, char name)
+{
+	int			i;
+	t_stack		*stk;
+	t_element	*elements;
 
 	stk = NULL;
-	while (*iarr)
+	i = -1;
+	elements = (t_element *)ft_calloc(n, sizeof(t_element *));
+	while (iarr[++i])
+		elements[i] = *ps_elenew(iarr[i]);
+	return (elements);
+}
+
+int	ps_elecmp(void *str1, void *str2)
+{
+	int	s1;
+	int	s2;
+	
+	s1 =((t_element *)str1)->value;
+	s2 =((t_element *)str2)->value;
+	return ((int)s1 - (int)s2);
+}
+
+void	ps_putnbr(t_element *ele)
+{
+	ft_putnbr_fd(ele->value, 1);
+	ft_putstr_fd(", ", 1);
+	ft_putnbr_fd(ele->sort, 1);
+	ft_putstr_fd("\n", 1);
+}
+
+void ps_print(t_stack stk)
+{
+	ft_lstiter(stk.head, ps_putnbr);
+	ft_putchar_fd('\n', 1);
+	btree_apply_infix(stk.root, ps_putnbr);
+	ft_putchar_fd('\n', 1);
+}
+
+int	ps_init_stacks(t_stack stks[2], t_element *elements, int n)
+{
+	//t_stack *tmp_a;
+	t_list	*tmplst;
+	int i;
+	
+	//*stks = (t_stack *)ft_calloc(sizeof(t_stack), 2);
+	(stks)[0].name = 'a';
+	(stks)[1].name = 'b';
+	i = -1;
+	while (++i < n)
+	{
+		tmplst = ft_lstnew(&elements[i]);
+		ft_lstadd_back(&stks[0].head, tmplst);
+		btree_insert_data(&stks[0].root, &elements[i], ps_elecmp);
+	}
+	stks[0].n = i;
 }
 
 int main(int ac, char **av)
 {
-	int	*iarr;
+	int			*iarr;
+	t_stack		stks[2];
+	t_element	*elements;
 
+	ft_bzero(stks, sizeof(stks));
 	if (ac > 1)
 	{
 		av++;
@@ -103,10 +168,17 @@ int main(int ac, char **av)
 		ac = chk_input(av);
 		iarr = ft_strstoiarr(av, ac);
 		if (iarr)
-			push_swap(iarr, ac);
+		{
+			elements = ps_getelements(iarr, ac, 'a');
+			if (!elements)
+				return (free(av), 0);
+		}
 		else if (ac == -1)
 			return (free(av), 0);
+		if (!ps_init_stacks(stks, elements, ac))
+			return (NULL);
 	}
+	ps_print(stks[0]);
 	return (1);
 
 }
